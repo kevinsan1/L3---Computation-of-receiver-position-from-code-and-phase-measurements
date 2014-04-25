bfa3320e63cb1f5338e0415d294dc1db21cb9db6
kevinsan1 <kevin.mead1@hotmail.com>
2014-04-25 16:36:40 +0200

    Import data and set to variables working

--- Lab3Calculations.m
+++ Lab3Calculations.m
@@ -3,34 +3,40 @@
 clear all; clc;
 %% Import file
 addpath('/Users/kevin/SkyDrive/KTH Work/Period 4 2014/GNSS/Labs/L3 - Computation of receiver position from code and phase measurements')
-DiffAssignmentL1 = xlsread('DiffAssignmentL1');
-cellDiff = num2cell(DiffAssignmentL1); % same as a cell
+mmainFile = xlsread('DiffAssignmentL1');
+mmainFile = padarray(mmainFile,5,'pre');
+cellDiff = num2cell(mmainFile); % same as a cell
 %% Constants
 c = 299792458; % speed of light (m/s)
+[lambda1,lambda2] = cellDiff{78:79,2};
 mu = 3.986005e14; % universal gravitational parameter (m/s)^3
 omega_e_dot = 7.2921151467e-5; % earth rotation rate (rad/s)
 F = -4.442807633e-10; % s/m^1/2
-nLineUpRows = 5;
+nRows = 5;
 %% Variables
-P1 = DiffAssignmentL1((6:22)-nLineUpRows,3);
-phi1 = DiffAssignmentL1((6:22)-nLineUpRows,4);
-dtA = DiffAssignmentL1(24-nLineUpRows,3);
-dtB = DiffAssignmentL1(25-nLineUpRows,3);
-rhoDot = DiffAssignmentL1((28:33)-nLineUpRows,2);
-satelliteNumbers = DiffAssignmentL1((28:33)-nLineUpRows,1);
-[Xref,Xrov,Yref,Yrov,Zref,Zrov] = cellDiff{(37:38)-nLineUpRows,3:5};
-%% Satellite Coordinates
-sat(20,:) = DiffAssignmentL1(43-nLineUpRows,3:5);
-sat(4,:) = DiffAssignmentL1(44-nLineUpRows,3:5);
-sat(5,:) = DiffAssignmentL1(45-nLineUpRows,3:5);
-sat(6,:) = DiffAssignmentL1(46-nLineUpRows,3:5);
-sat(24,:) = DiffAssignmentL1(47-nLineUpRows,3:5);
-sat(25,:) = DiffAssignmentL1(48-nLineUpRows,3:5);
-
-
+dtA = mmainFile(24,3);
+dtB = mmainFile(25,3);
+satelliteNumbers = mmainFile((28:33),1);
+[Xref,Xrov,Yref,Yrov,Zref,Zrov] = cellDiff{(37:38),3:5};
+for i = 43:48 % 
+    rhoDot(mmainFile(i-15,1),1) = mmainFile(i-15,2);
+    satCoordinates(mmainFile(i,1),:) = mmainFile(i,3:5);
+    rhoAtoP(mmainFile(i+23,1),:) = mmainFile(i+23,2);
+    rhoB0toP(mmainFile(i+23,1),:) = mmainFile(i+23,3);
+end
+count = 1;
+for i = 6:3:21
+   P1ref(satelliteNumbers(count),1) = mmainFile(i,3);
+   P1rov(satelliteNumbers(count),1) = mmainFile(i+1,3);
+   phi1ref(satelliteNumbers(count),1) = mmainFile(i,4);
+   phi1rov(satelliteNumbers(count),1) = mmainFile(i+1,4);
+   count = count + 1;
+end
+weightMatrix = mmainFile(52:61,1:10);
 %% Main steps of Calculation
 %% 1. Synchronize observables from both receivers using equations
-% (1
+% 14
+
 %% 2. Compute single and double differences ? equations (15) and
 % (18). Use satellite 20 as a
 % AB,0
