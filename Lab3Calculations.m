0d85f32e4f8862a5c3f717d9a96ff0e1fc41235d
kevinsan1 <kevin.mead1@hotmail.com>
2014-04-25 17:26:04 +0200

    added vector of ones

--- Lab3Calculations.m
+++ Lab3Calculations.m
@@ -3,9 +3,9 @@
 clear all; clc;
 %% Import file
 addpath('/Users/kevin/SkyDrive/KTH Work/Period 4 2014/GNSS/Labs/L3 - Computation of receiver position from code and phase measurements')
-mmainFile = xlsread('DiffAssignmentL1');
-mmainFile = padarray(mmainFile,5,'pre');
-cellDiff = num2cell(mmainFile); % same as a cell
+excfile = xlsread('DiffAssignmentL1');
+excfile = padarray(excfile,5,'pre');
+cellDiff = num2cell(excfile); % same as a cell
 %% Constants
 c = 299792458; % speed of light (m/s)
 [lambda1,lambda2] = cellDiff{78:79,2};
@@ -14,37 +14,42 @@ omega_e_dot = 7.2921151467e-5; % earth rotation rate (rad/s)
 F = -4.442807633e-10; % s/m^1/2
 nRows = 5;
 %% Variables
-dtA = mmainFile(24,3);
-dtB = mmainFile(25,3);
-satelliteNumbers = mmainFile((28:33),1);
+dtA = excfile(24,3);
+dtB = excfile(25,3);
+sNum = excfile((28:33),1);
+for i = 1:length(sNum) % vector of ones at satellite numbers
+    vectorOfOnes(sNum(i),1) = 1; 
+end
 [Xref,Xrov,Yref,Yrov,Zref,Zrov] = cellDiff{(37:38),3:5};
 for i = 43:48 % 
-    rhoDot(mmainFile(i-15,1),1) = mmainFile(i-15,2);
-    satCoordinates(mmainFile(i,1),:) = mmainFile(i,3:5);
-    rhoAtoP(mmainFile(i+23,1),:) = mmainFile(i+23,2);
-    rhoB0toP(mmainFile(i+23,1),:) = mmainFile(i+23,3);
+    rhoDot(excfile(i-15,1),1) = excfile(i-15,2);
+    satCoordinates(excfile(i,1),:) = excfile(i,3:5);
+    rhoAtoP(excfile(i+23,1),:) = excfile(i+23,2);
+    rhoB0toP(excfile(i+23,1),:) = excfile(i+23,3);
 end
 count = 1;
 for i = 6:3:21
-   P1ref(satelliteNumbers(count),1) = mmainFile(i,3);
-   P1rov(satelliteNumbers(count),1) = mmainFile(i+1,3);
-   phi1ref(satelliteNumbers(count),1) = mmainFile(i,4);
-   phi1rov(satelliteNumbers(count),1) = mmainFile(i+1,4);
+   P1ref(sNum(count),1) = excfile(i,3);
+   P1rov(sNum(count),1) = excfile(i+1,3);
+   phi1ref(sNum(count),1) = excfile(i,4);
+   phi1rov(sNum(count),1) = excfile(i+1,4);
    count = count + 1;
 end
-weightMatrix = mmainFile(52:61,1:10);
+weightMatrix = excfile(52:61,1:10);
 %% Main steps of Calculation
 %% 1. Synchronize observables from both receivers using equations
 % 14
 P1refeq14 = P1ref + rhoDot*dtA;
 P1roveq14 = P1rov + rhoDot*dtB;
-phi1refeq14 = lambda1*phi1ref + rhoAtoP * dtA; % lambda times phi
-phi1roveq14 = lambda1*phi1rov + rhoAtoP * dtB; % lamda times phi
-%% 2. Compute single and double differences ? equations (15) and
-% (18). Use satellite 20 as a
-% AB,0
-% i dd
-% reference satellite for double differencing.
+phi1refeq14 = phi1ref + 1/lambda1 * rhoAtoP * dtA;  
+phi1roveq14 = phi1rov + 1/lambda1 * rhoAtoP * dtB;  
+%% 2. Compute single and double differences - equations (15) and
+% (18). Use satellite 20 as a reference satellite
+% for double differencing.
+P1eq15 = P1refeq14 - P1roveq14;
+phi1eq15 = (phi1refeq14 - phi1roveq14);
+dtAB = dtB - dtA;
+% Double differences
 
 %% 3. Compute coefficients a , a , a and ?pq - equations (12).
 % XYZ
